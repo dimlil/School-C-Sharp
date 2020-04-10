8771f3d968c2c985064c71f1a5d71d938e192bf8
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-04-10 15:24:41 +0300

    Added old project

---
+++ 11 grade/ArrayList Test/ArrayList Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            Console.WriteLine("Create");
+            for (int i = 0; i <= 10; i++)
+            {
+                arrayList.Add(i);
+            }
+            foreach (int count in arrayList) 
+            {
+                Console.WriteLine(arrayList[count]);
+            }
+            Console.WriteLine("Add");
+            for (int i = 0; i <= 5; i++)
+            {
+                arrayList.Add(i);
+            }
+            foreach (int count in arrayList)
+            {
+                Console.WriteLine(arrayList[count]);
+            }
+            Console.WriteLine("remove");
+            arrayList.RemoveRange(14,3);
+            foreach (int count in arrayList)
+            {
+                Console.WriteLine(arrayList[count]);
+            }
+        }
+    }
+}
