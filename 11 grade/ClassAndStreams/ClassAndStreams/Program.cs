b9c88f1ac91e55eef25d182d8a2b4e072446988c
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-06-03 02:07:09 +0300

    Added .txt files used in stream homework tasks and projects

---
+++ 11 grade/ClassAndStreams/ClassAndStreams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ClassAndStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw=new StreamWriter(@"./something.txt",true))
+            {
+                sw.Write("Helloooo");
+            }
+        }
+    }
+}
